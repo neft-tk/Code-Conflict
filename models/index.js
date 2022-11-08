cf0f3fe959a7d1a4186d7d1499ac1a9408db5a62
unknown <ebussman@gmail.com>
2022-11-08 13:19:52 -0800

    Added Move.js and updated the index.js in models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Dev = require('./Dev');
+const Move = require('./Move');
 
 User.hasMany(Dev, {
   foreignKey: 'user_id',
@@ -7,6 +8,15 @@ User.hasMany(Dev, {
 });
 
 Dev.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Dev.hasMany(Move, {
+  foreignKey: 'move_id',
+  onDelete: 'CASCADE'
+});
+
+Move.belongsTo(Dev, {
   foreignKey: 'user_id'
 });
 
